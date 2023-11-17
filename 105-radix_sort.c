182743a9b09f4b6bd63f13797fc0a34315a78702
CletsyMedia <Cletussam12@yahoo.com>
2023-11-17 02:02:50 +0100

    Radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -43,17 +43,18 @@ int get_maximum(int *array, int size)
 void radix_counter_sort(int *array, size_t size, int sig_dig, int *buffs)
 {
 	int counter[10] = {0};
+	size_t i;
 
 	/* Count occurrences of each digit at the current significant digit */
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	counter[(array[i] / sig_dig) % 10]++;
 
 	/* Update counter array to store the actual position of each digit */
-	for (int i = 1; i < 10; i++)
+	for (i = 1; i < 10; i++)
 	counter[i] += counter[i - 1];
 
 	/* Build the sorted array using the counter array */
-	for (int i = size - 1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 	int digit = (array[i] / sig_dig) % 10;
 
@@ -61,7 +62,7 @@ void radix_counter_sort(int *array, size_t size, int sig_dig, int *buffs)
 	}
 
 	/* Copy the sorted elements back to the original array */
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	array[i] = buffs[i];
 }
 
@@ -81,17 +82,17 @@ void radix_sort(int *array, size_t size)
 	int maxim, sig_dig, *buffs;
 
 	if (array == NULL || size < 2)
-		return;
+	return;
 
 	buffs = malloc(sizeof(int) * size);
 	if (buffs == NULL)
-		return;
+	return;
 
 	maxim = get_maximum(array, size);
 	for (sig_dig = 1; maxim / sig_dig > 0; sig_dig *= 10)
 	{
-		radix_counter_sort(array, size, sig_dig, buffs);
-		print_array(array, size);
+	radix_counter_sort(array, size, sig_dig, buffs);
+	print_array(array, size);
 	}
 
 	free(buffs);
