998885439631a9c5d56f29dd3f13bb1285cd982b
CletsyMedia <Cletussam12@yahoo.com>
2023-11-17 01:25:50 +0100

    Updating headers

--- sort.h
+++ sort.h
@@ -37,7 +37,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms */
+/* Sorting algoritms prototypes*/
+
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
@@ -50,6 +51,5 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 
 #endif /* SORT_H */
