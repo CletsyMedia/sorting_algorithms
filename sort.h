0c64a6db8207e36a224fe08316f9996e55a38b35
Cletus Samuel <109830247+CletsyMedia@users.noreply.github.com>
2023-11-18 22:44:43 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -53,6 +53,6 @@ void shell_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 /* Swapping nodes in a doubly linked list */
-void swap_integers(listint_t *a, listint_t **b);
+void swap_integers(int *a, int *b)
 
 #endif /* SORT_H */
